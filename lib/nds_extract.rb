390ba7ff54a4ea1bf3530b15178050858ff8a002
Mitchell T Alton <mitchell.alton@live.com>
2020-01-24 03:13:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
+require 'pp'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
@@ -48,6 +49,14 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  
+  movies_collection = []
+  i = 0 
+  while i < name.length do 
+    movies_collection << name[i]
+    i += 1 
+  end
+  movies_collection
 end
 
 
